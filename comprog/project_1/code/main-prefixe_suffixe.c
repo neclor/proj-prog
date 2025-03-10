6a096ad2cc5e0d52ba2749dc4d4938f7ee7551ff
neclor <neclordev@gmail.com>
2025-03-10 22:45:08 +0100

    abcd

--- comprog/project_1/code/main-prefixe_suffixe.c
+++ comprog/project_1/code/main-prefixe_suffixe.c
@@ -7,11 +7,14 @@ int main(){
    const unsigned int N1 = 9;
    const unsigned int N2 = 10;
    const unsigned int N3 = 9;
-   int T1[9] = {1,4,2,4,5,1,4,2,4}; // 4
-   int T2[10] = {1,2,3,2,1,1,2,3,2,1}; // 5
-   int T3[9] = {3,2,3,2,1,2,3,2,1}; // 0
+   int T1[9] = {1,4,2,4,5,1,4,2,4};
+   int T2[10] = {1,2,3,2,1,1,2,3,2,1};
+   int T3[9] = {3,2,3,2,1,2,3,2,1};
+   int TM[17] = {1,2,3,4,1,2,3,1,2,3,4,1,2,3,4,1,2};
 
-   printf("Longueur plus long préfixe/suffixe de T1: %u\n", prefixe_suffixe(T1, N1));
-   printf("Longueur plus long préfixe/suffixe de T2: %u\n", prefixe_suffixe(T2, N1));
-   printf("Longueur plus long préfixe/suffixe de T3: %u\n", prefixe_suffixe(T3, N1));
+   printf("Longueur plus long préfixe/suffixe de T1: %u\n", prefixe_suffixe(T1, N1)); // 4
+   printf("Longueur plus long préfixe/suffixe de T2: %u\n", prefixe_suffixe(T2, N2)); // 5
+   printf("Longueur plus long préfixe/suffixe de T3: %u\n", prefixe_suffixe(T3, N3)); // 0
+
+   printf("my test: %u\n", prefixe_suffixe(TM, 17)); // 6
 }
--- comprog/project_1/code/prefixe_suffixe.c
+++ comprog/project_1/code/prefixe_suffixe.c
@@ -5,30 +5,59 @@
 
 #include <stdio.h>
 
+
+/*
+ * Explication:
+ * 
+ *  
+ * a b a b x a b a b b
+ * 0 0 1 2 0 1 2 3 4 0 
+ * 
+ * a b a b
+ * 0 0 1 2
+ * 
+*/
+
 int prefixe_suffixe(int *T, const unsigned int N) {
    int p[N];
    unsigned k = 0;
 
-   // initialize with 0
-   for (unsigned i = 1; i < N; ++i) {
+   // initialize p[N] with 0
+   for (unsigned i = 0; i < N; ++i) {
       p[i] = 0;
    }
 
-   printf("%d, ", p[0]);
+//     ------------- ------------- 
+// T = a b c d a b c a b c d a b c d a b
+// p = 0 0 0 0 1 2 3 1 2 3 4 5 6 7 4
 
-   for (unsigned i = 1; i < N; i++) {
+   unsigned int i = 1;
+   while (i < N) {
       if (T[i] == T[k]) {
-         p[i] = ++k;
+         ++k;
+         p[i] = k;
+         ++i;
       } else {
-         while (k > 0 && T[i] != T[k]) 
+         if (k == 0) {
+            p[i] = 0;
+            ++i;
+         } else {
             k = p[k - 1];
+         }
       }
-
-      printf("%d, ", p[i]);
    }
 
+   for (unsigned i = 0; i < N; ++i) {
+      printf("%d, ", p[i]);
+   }
 
    return p[N-1];
 }
 
 // gcc code/main-prefixe_suffixe.c code/prefixe_suffixe.c
+
+// -----   -----
+// a b c d a b c a b c d a b c d a b
+// 
+// 0 0 0 0 1 2 3 1 2 3 4 5 6 7 4 5 6
+//
